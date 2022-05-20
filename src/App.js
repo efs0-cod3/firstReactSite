f751cf0a5e649b800fff28e8d8a2c2a3f3ec4393
efs0-cod3 <eflearnng@gmail.com>
2022-05-19 20:02:49 -0400

    mod

--- src/App.js
+++ src/App.js
@@ -1,28 +1,31 @@
-
+import qrCode from './assets/image-qr-code.png'
 
 
 function App() {
   return ( 
     <div>
-    <main >
-    <h2>
-    Improve your front - end skills by building projects
-    </h2>
-    <p>
-    Scan the QR code to visit Frontend Mentor and take your coding skills to the next level 
-    </p>
+      <main>
+        <div className="card">
+          <img className='qrImg' src= {qrCode} alt='qr-code'/>
+          <div className="container">
+            <h1>
+            Improve your front - end skills by building projects
+            </h1>
+            <p>
+            Scan the QR code to visit Frontend Mentor and take your coding skills to the next level 
+            </p>
+          </div>
+      </div>
     </main>
-    
-    <div class = "attribution" >
-    <p>
-    Challenge by <a href = "https://www.frontendmentor.io?ref=challenge" >
-    Frontend Mentor 
-    </a>. 
-    </p>
-    <p>
-    Coded by Erick 'efs0' Felix.
-    </p> 
-    </div>
+
+      <div className= "attribution">
+        <p>
+          Challenge by <a href = "https://www.frontendmentor.io?ref=challenge" >
+          Frontend Mentor 
+          </a>. 
+        </p>
+        <p>Coded by Erick 'efs0-cod3' Felix.</p>
+      </div>
     </div>
   );
 }
